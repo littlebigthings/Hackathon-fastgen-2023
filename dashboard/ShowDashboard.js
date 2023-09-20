7b955b5962810d356a88e550619852286456ce07
Himanshu Raj Verma <himanshurajverma549@gmail.com>
2023-09-20 12:58:16 +0530

    added code to generate new dashboard +  copy functionality on meta page

--- dashboard/ShowDashboard.js
+++ dashboard/ShowDashboard.js
@@ -1,9 +1,12 @@
 import checkToken from "../loginSignup/CheckToken.js";
 import fetchApiData from "../loginSignup/APIcall.js";
-
+import addLogout from "./logout.js";
+let loader;
 function showDashBoard() {
     let jwtToken = checkToken("jwtToken");
     let url = "https://metatags-generator.fastgenapp.com/get-site-info";
+    loader = document.querySelector("[wrapper='loader-dark']");
+    loader?.classList.remove("hide-wrapper");
     let options = {
         "method": "POST",
         "content-type": "application/json",
@@ -17,11 +20,15 @@ function showDashBoard() {
     }
     
     fetchApiData(url, options)
-        .then((data) => {
-            console.log("API Data:", data);
-            let checkSites = data.siteData?.Data?.Response;
+        .then(async (data) => {
+            // console.log("API Data:", await data.json());
+            let checkSites = data?.siteData;
             if (checkSites?.length > 0) {
                 showSites(checkSites)
+            }
+            else{
+                let checkError = await data.json();
+                showNoSitesFound(checkError);
             }
 
         })
@@ -34,17 +41,22 @@ function showDashBoard() {
 function showSites(sitesArray) {
     let parentWrapper = document.querySelector("[wrapper='site-info']").parentElement;
     let siteInfoWrapper = document.querySelector("[wrapper='site-info']");
-    let name, image, showMetaBtn, showSchemaBtn;
+    loader?.classList.add("hide-wrapper");
+    let name, image, showMetaBtn, showSchemaBtn, showMissingSchema, showMissingMeta;
     if (siteInfoWrapper != undefined) {
         sitesArray.forEach(site => {
-            let { displayName, previewUrl, id, customDomains, shortName } = site;
+            let { displayName, previewUrl, id, customDomains, shortName, metaTagsCount, schemaCount} = site.Data;
             let clonedWrapper = siteInfoWrapper.cloneNode(true);
             name = clonedWrapper.querySelector("[show-data='name']");
             image = clonedWrapper.querySelector("[show-data='img']");
             showSchemaBtn = clonedWrapper.querySelector("[show-data='pages-schema']");
             showMetaBtn = clonedWrapper.querySelector("[show-data='pages-meta']");
+            showMissingMeta = clonedWrapper.querySelector("[show-missing='meta']");
+            showMissingSchema = clonedWrapper.querySelector("[show-missing='schema']");
             name.textContent = displayName;
             image.setAttribute("src", previewUrl);
+            showMissingMeta.textContent = metaTagsCount;
+            showMissingSchema.textContent = schemaCount;
             if (customDomains.length > 0) {
                 showSchemaBtn.setAttribute("href", `/site-pages-schema?siteId=${id}&url=${customDomains[0].url}`);
                 showMetaBtn.setAttribute("href", `/site-pages-meta?siteId=${id}&url=${customDomains[0].url}`);
@@ -52,9 +64,17 @@ function showSites(sitesArray) {
                 showSchemaBtn.setAttribute("href", `/site-pages-schema?siteId=${id}&url=${shortName}.webflow.io`);
                 showMetaBtn.setAttribute("href", `/site-pages-meta?siteId=${id}&url=${shortName}.webflow.io`);
             }
+            clonedWrapper.classList.remove("hide-wrapper");
 
             parentWrapper.appendChild(clonedWrapper);
         })
     }
 }
+
+function showNoSitesFound(){
+    let noSitesFoundWrapper = document.querySelector("[wrapper='no-sites']");
+    noSitesFoundWrapper?.classList.remove("hide-wrapper");
+    loader.classList.add("hide-wrapper");
+}
 showDashBoard();
+addLogout();
--- dashboard/ShowPagesMeta.js
+++ dashboard/ShowPagesMeta.js
@@ -2,19 +2,20 @@
 
 import fetchApiData from "../loginSignup/APIcall.js";
 import checkToken from "../loginSignup/CheckToken.js";
-import showError from "../loginSignup/ShowError.js";
+import addLogout from "./logout.js";
+// import showError from "../loginSignup/ShowError.js";
 
 function loadPages() {
     let darkLoader = document.querySelector("[wrapper=loader-dark]");
     let pageURl = new URL(document.location);
     let siteId = pageURl?.searchParams.get("siteId");
     let siteUrl = pageURl?.searchParams.get("url");
-    
+
     let jwtToken = checkToken("jwtToken");
-    if(jwtToken == null){
+    if (jwtToken == null) {
         window.location.assign("/")
     }
-    
+
     darkLoader.classList.remove("hide-wrapper")
 
     let url = "https://metatags-generator.fastgenapp.com/get-pages";
@@ -49,22 +50,24 @@ function showPages(pagesData) {
     let metaWrapperToClone = document.querySelector("[meta-wrapper='to-clone']");
     let metaWrapperToInject = document.querySelector("[meta-wrapper='inject']");
     let loader = document.querySelector("[wrapper='loader']");
-    let clonedMetaWrapper, pageNumber, pageName, metaTitleText, metaDescriptionText, generateNewBtn, metaInitialInfoWrapper, clonedLoader, metaGeneratedInfoWrapper, btnText, spanElement, closeBtn;
+    let clonedMetaWrapper, pageNumber, pageName, metaTitleText, metaDescriptionText, generateNewBtn, metaInitialInfoWrapper, clonedLoader, metaGeneratedInfoWrapper, btnText, spanElement, closeBtn, sameMetaTitleTick, sameMetaDescriptionTick;
     if (pagesData?.length > 0) {
         pagesData.forEach((page, index) => {
-            let { metaDescription, metaTitle, name, pageUrl } = page;
+            let { metaDescription, metaTitle, name, pageUrl, ogSameDescription, ogSameTitle } = page;
             if (metaTitle == undefined && metaDescription == undefined && name == undefined && pageUrl == undefined) return;
             clonedMetaWrapper = metaWrapperToClone.cloneNode(true);
             clonedLoader = loader.cloneNode(true);
             clonedLoader.classList.add("hide-wrapper");
-            
+
             metaInitialInfoWrapper = clonedMetaWrapper?.querySelector("[meta-wrapper='initial']");
             metaGeneratedInfoWrapper = clonedMetaWrapper?.querySelector("[meta-wrapper='generated']");
             pageNumber = clonedMetaWrapper?.querySelector("[meta='number']");
             pageName = clonedMetaWrapper?.querySelector("[meta='name']");
             metaTitleText = clonedMetaWrapper?.querySelectorAll("[meta='title']");
             metaDescriptionText = clonedMetaWrapper?.querySelectorAll("[meta='description']");
-            
+            sameMetaTitleTick = metaInitialInfoWrapper?.querySelector("[meta='title-same']");
+            sameMetaDescriptionTick = metaInitialInfoWrapper?.querySelector("[meta='description-same']");
+
             generateNewBtn = clonedMetaWrapper?.querySelector("[meta='generate']");
             spanElement = document.createElement("span");
             btnText = generateNewBtn.textContent;
@@ -72,11 +75,15 @@ function showPages(pagesData) {
             spanElement.textContent = btnText;
             generateNewBtn.appendChild(spanElement);
             generateNewBtn.appendChild(clonedLoader);
+            generateNewBtn.setAttribute("regenerate", false);
 
             closeBtn = clonedMetaWrapper?.querySelector("[meta='close']");
 
             pageNumber.textContent = index;
             pageName.textContent = name;
+
+            ogSameTitle ? sameMetaTitleTick.style.display = "block" : sameMetaTitleTick.style.display = "none";
+            ogSameDescription ? sameMetaDescriptionTick.style.display = "block" : sameMetaDescriptionTick.style.display = "none";
             generateNewBtn.setAttribute("page-url", `https://${pageUrl}`);
             if (metaTitleText?.length > 0) {
                 metaTitleText.forEach(title => {
@@ -113,43 +120,67 @@ function addGenerateListener(generateNewBtn, wrapper, loader, spanElement, close
     let closeCta = closeBtn;
     let generatedTitleToShow = wrapperToshow?.querySelector("[meta='generated-title']");
     let generatedDescriptionToShow = wrapperToshow?.querySelector("[meta='generated-description']");
+    let copyTitleBtn = wrapperToshow?.querySelector("[copy='title']");
+    let copyDescriptionBtn = wrapperToshow?.querySelector("[copy='description']");
 
-    generateBtn.addEventListener("click",async (evt)=>{
+    generateBtn.addEventListener("click", async (evt) => {
         loaderWrapper.classList.remove("hide-wrapper");
         btnText.style.display = "none";
         let clickedOn = evt.currentTarget;
         let selectedPage = clickedOn?.getAttribute("page-url");
+        let regenerate = clickedOn?.getAttribute("regenerate");
         console.log(selectedPage);
-        let pageMetaData = await loadPageNewMeta(selectedPage);
+        let pageMetaData = await loadPageNewMeta({ selectedPage, regenerate });
         console.log(pageMetaData)
-        if(pageMetaData?.error){
+        if (pageMetaData?.error) {
             console.log(pageMetaData.error)
         }
-        else if(pageMetaData?.metaTags?.Content){
-            let {Content} = pageMetaData?.metaTags;
+        else if (pageMetaData?.metaTags?.Content) {
+            let { Content } = pageMetaData?.metaTags;
             let dataJson = JSON.parse(Content);
-            let {metaTitle, metaDescription} = dataJson;
+            let { metaTitle, metaDescription } = dataJson;
             generatedTitleToShow.textContent = metaTitle;
             generatedDescriptionToShow.textContent = metaDescription;
             btnText.style.display = "inline-block";
             loaderWrapper.classList.add("hide-wrapper");
             wrapperToshow.classList.remove("hide-wrapper");
             btnText.textContent = "Regenerate";
+            copyTitleBtn.setAttribute("data-clipboard-text", metaTitle);
+            copyDescriptionBtn.setAttribute("data-clipboard-text", metaDescription);
+            clickedOn.setAttribute("regenerate", true);
         }
     })
-     closeCta.addEventListener("click",()=>{
+    // add copyListener
+    addCopyListener(copyTitleBtn);
+    addCopyListener(copyDescriptionBtn);
+
+    closeCta.addEventListener("click", () => {
         wrapperToshow.classList.add("hide-wrapper");
-     })
+    })
 }
 
-async function loadPageNewMeta(pageurl) {
+function addCopyListener(copyBtn) {
+    let cta = copyBtn;
+
+    let clipboard = new ClipboardJS(cta);
+
+    clipboard.on('success', function (e) {
+        let currentTarget = e.trigger;
+        let lastTitle = currentTarget?.textContent;
+        e.trigger.textContent = "Copied!"
+        setTimeout(() => currentTarget.textContent = lastTitle, 1000)
+    });
+}
+
+async function loadPageNewMeta(pageData) {
 
     let url = "https://metatags-generator.fastgenapp.com/get-meta-tags";
     let options = {
         "method": "POST",
         "content-type": "application/json",
         "body": JSON.stringify({
-            "pageUrl": pageurl
+            "pageUrl": pageData.selectedPage,
+            "regenerate": pageData.regenerate
         })
     }
     return await fetchApiData(url, options)
@@ -168,3 +199,4 @@ async function loadPageNewMeta(pageurl) {
 
 
 loadPages();
+addLogout();
